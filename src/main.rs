8b8aad37f8cb89d659a77660764ee63057f6fce0
Jessica <greasetognd>
2025-11-21 10:20:04 -0500

    WORK BITCH

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
-// love u sed <3 
-mod info;
-fn main()
-{
-    println!("My Fetch");
-    println!("Uptime : {}", info::uptime());
-    //println!("love u sed gros bisous");
-    //bruh
-=======
-mod info;
 
-fn main()
-{
->>>>>>> f1248fb2904e534a52b6aeaedf1a0c5ca76b586e
-    //todo list
-    //- faire le ascii
-    //- faire les infos
-}
