1d45c766b3549ebffbd3a25c4d2826adeb9cbce4
Jessica <greasetognd>
2025-11-21 10:05:33 -0500

    uptime did i think

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
 // love u sed <3 
-use std::process::Command;
+mod info;
 fn main()
 {
-    let OS = get_os();
-    let UPTIME = get_uptime();
-    let SHELL = get_shell();
-    let KERNEL = get_kernel();
-
-    println!("==WELCOME TO JSFETCH==");
-    println!("--Brought to you by Jessica and Sedrick--");
-    println!("Current OS: {}", OS);
-    println!("Uptime: {}", UPTIME);
-    println!("Shell: {}", SHELL);
-    println!("Kernel: {}", KERNEL);
-
-    fn get_os() -> String {
-        "Unknown_OS".to_string()
-    }
-    fn get_uptime() -> String {
-        "Unknown_Uptime".to_string()
-    }
-    fn get_shell() -> String {
-        "Unknown_Shell".to_string()
-    }
-    fn get_kernel() -> String {
-        "Unknown_Kernel".to_string()
-    }
-
+    println!("My Fetch");
+    println!("Uptime : {}", info::uptime());
     //println!("love u sed gros bisous");
     //bruh
     //todo list
