7e74ef43540de9b8a0c9f3df65fb1c9135de7d1f
Jessica <greasetognd>
2025-11-21 09:20:04 -0500

    YAY

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 // love u sed <3 
+use std::process::Command;
 fn main()
 {
-    println!("love u sed gros bisous");
+    let OS = get_os();
+    let UPTIME = get_uptime();
+    let SHELL = get_shell();
+    let KERNEL = get_kernel();
+
+    println!("==WELCOME TO JSFETCH==");
+    println!("--Brought to you by Jessica and Sedrick--");
+    println!("Current OS: {}", OS);
+    println!("Uptime: {}", UPTIME);
+    println!("Shell: {}", SHELL);
+    println!("Kernel: {}", KERNEL);
+
+    fn get_os() -> String {
+        "Unknown_OS".to_string()
+    }
+    fn get_uptime() -> String {
+        "Unknown_Uptime".to_string()
+    }
+    fn get_shell() -> String {
+        "Unknown_Shell".to_string()
+    }
+    fn get_kernel() -> String {
+        "Unknown_Kernel".to_string()
+    }
+
+    //println!("love u sed gros bisous");
     //bruh
     //todo list
     //- faire le ascii
