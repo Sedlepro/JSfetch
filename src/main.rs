aeed99a269fb37d0b6fd9c113f1b6da246489670
Sedlepro <104863899+Sedlepro@users.noreply.github.com>
2025-11-22 12:35:29 -0500

    user done (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod info;
 
 fn main() {
+    let username = info::user::get_username();
+    println!("User: {}", username);
+
     let uptime = info::uptime::uptime();
     println!("Uptime: {} ", uptime);
-
+    
 }
