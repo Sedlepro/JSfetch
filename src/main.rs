67b34afaf908a3ef8d7135cc225f3dd87de041dc
Jessica <greasetognd>
2025-11-25 09:59:12 -0500

    terminal fini!

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod info;
 fn main() {
     let uptime = info::uptime::uptime();
     println!("Uptime: {} ", uptime);
-
+    let term = info::terminal::terminal();
+    println!("Terminal: {}", term);
 }
